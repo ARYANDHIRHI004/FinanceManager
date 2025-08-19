e14eb304c6f858205e246852ef52e671a53ce3a6
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-08-19 22:05:39 +0530

    feat: project models created.

--- backend/src/models/project.models.js
+++ backend/src/models/project.models.js
@@ -1,5 +1,29 @@
 import mongoose from "mongoose";
 
-const projectSchema = new mongoose.Schema({},{timestamps:true})
+const projectSchema = new mongoose.Schema(
+  {
+    accountId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Account",
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    projectName: {
+      type: String,
+      required: true,
+    },
+    projectDesc: {
+      type: String,
+      required: true,
+    },
+    projectBudget: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-export const Project = mongoose.model("Project", projectSchema)
+export const Project = mongoose.model("Project", projectSchema);
