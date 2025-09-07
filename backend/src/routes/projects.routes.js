d20e7736b7dc4c598a8b9a58a98b8a5e7fcc2ed8
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-09-07 09:10:20 +0530

    fix: middlewares added in project route file to check user login and project member role

--- backend/src/routes/projects.routes.js
+++ backend/src/routes/projects.routes.js
@@ -11,35 +11,39 @@ import {
   updateProject,
   updateRoleOfProjectMembers,
 } from "../controllers/projects.controllers.js";
+import { checkAccountMemberRole, verifyJWT } from "../middlewares/auth.middlewares.js";
+import { checkAccountType } from "../middlewares/account.middlewares.js";
+import { checkSubscription } from "../middlewares/subscription.middlewares.js";
+import { checkProjectMemberRole } from "../middlewares/project.middlewares.js";
 
 const projectRouter = Router();
 
-projectRouter.route("/create-project/:accountId").post(createProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
+projectRouter.route("/create-project/:accountId").post(verifyJWT, checkSubscription(["Premium_Plan"]), checkAccountType(["joint"]), checkAccountMemberRole(["ADMIN", "COLLABORATER"]), createProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
 
-projectRouter.route("/get-all-projects/:accountId").get(getAllProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
+projectRouter.route("/get-all-projects/:accountId").get(verifyJWT, getAllProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
 
-projectRouter.route("/get-my-projects/:accountId").get(getMyProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
+projectRouter.route("/get-my-projects/:accountId").get(verifyJWT, getMyProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
 
-projectRouter.route("/get-project-by-id/:projectId").get(getProjectById); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
+projectRouter.route("/get-project-by-id/:projectId").get(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Member", "Project_Admin"]), getProjectById); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
 
-projectRouter.route("/update-project/:projectId").put(updateProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
+projectRouter.route("/update-project/:projectId").put(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Admin"]), updateProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
 
-projectRouter.route("/delete-project/:projectId").delete(deleteProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
+projectRouter.route("/delete-project/:projectId").delete(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Admin"]), deleteProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater,
 
 projectRouter
   .route("/add-members-to-project/:projectId")
-  .post(addMemberToProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
+  .post(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Admin"]), addMemberToProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
 
 projectRouter
   .route("/remove-members-from-project/:projectId")
-  .delete(removeMemberFromProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
+  .delete(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Admin"]), removeMemberFromProject); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
 
 projectRouter
   .route("/get-all-project-members/:projectId")
-  .get(getAllProjectMembers); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
+  .get(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Admin"]), getAllProjectMembers); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
 
 projectRouter
   .route("/update-role-of-project-members/:projectId")
-  .get(updateRoleOfProjectMembers); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
+  .get(verifyJWT, checkAccountType(["joint"]), checkProjectMemberRole(["Project_Admin"]), updateRoleOfProjectMembers); // middlewares - verifyJW, subscription-premium, account-type, account-admin or collaborater, project-admin
 
 export default projectRouter;
